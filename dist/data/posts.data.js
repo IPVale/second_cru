b245cd14e5e96644ca549c382caaa29c765d6325
IPVale <valeria.imbrogiop@gmail.com>
2024-02-26 16:42:00 +0100

    feature(): created a user crud on array source

---
+++ dist/data/posts.data.js
@@ -0,0 +1,23 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.users = void 0;
+exports.users = [
+    {
+        id: "1",
+        name: {
+            firstName: "Mario",
+            secondName: "Rossi"
+        },
+        mail: "mario.rossi@example.com",
+        password: "password123"
+    },
+    {
+        id: "2",
+        name: {
+            firstName: "Luigi",
+            secondName: "Verdi"
+        },
+        mail: "luigi.verdi@example.com",
+        password: "password456"
+    },
+];
